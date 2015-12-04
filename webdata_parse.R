6c3c537acf8c9785fe833462616812718c6e0115
skylerroh <skylerroh@gmail.com>
2015-12-04 11:35:51 -0800

    webparsing to named ts

--- webdata_parse.R
+++ webdata_parse.R
@@ -1,3 +1,19 @@
 library(XML)
-theurl <- "http://www.census.gov/econ/currentdata/dbsearch?program=RESSALES&startYear=1963&endYear=2015&categories=SOLD&dataType=TOTAL&geoLevel=WE&notAdjusted=1&errorData=1&submit=GET+DATA"
-tables <- readHTMLTable(theurl, header = F)[[1]]
+library(astsa)
+
+url = "http://www.census.gov/econ/currentdata/dbsearch?program=RESSALES&startYear=1963&endYear=2015&categories=SOLD&dataType=TOTAL&geoLevel=WE&notAdjusted=1&errorData=1&submit=GET+DATA"
+tables = readHTMLTable(url, header = T)[[1]]
+rownames(tables) = as.character(tables[['Year']])
+tables = t(tables[,-1])
+names = character()
+for (j in 1:ncol(tables)){ 
+  for (i in 1:nrow(tables)){
+    names = c(names, paste(dimnames(tables)[[1]][i], dimnames(tables)[[2]][j], sep = " "))
+  }
+}
+vec = as.vector(unlist(tables))
+names(vec) = names
+vec = vec[!is.na(as.numeric(vec))]
+west = ts(vec, frequency = 12, start = c(1973, 1)) 
+west
+plot(west)
